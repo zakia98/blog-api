a9d88a1cf9fb3fa86738f5e5a426398bf0338f6c
Ameen <ameenahmed1998@gmail.com>
2022-05-09 15:46:42 +0100

    Outlined routes for CRUD actions.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/posts', function(req, res, next) {
+  //works
+  res.send('getting all posts')
+});
+
+router.post('/posts', function(req, res, next) {
+  //works
+  res.send('uploading a new post')
+});
+
+router.get('/posts/:id', function(req, res, next) {
+  //
+  res.send(`Getting the post for ID ${req.params.id} `)
+})
+
+router.delete('/posts/:id', function(req, res, next) {
+  res.send(`deleting post id ${req.params.id}`)
+})
+
 
 module.exports = router;

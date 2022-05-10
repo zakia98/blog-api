6bae974c18d6d9187e84fad3d1d7e053bf76bae3
Ameen <ameenahmed1998@gmail.com>
2022-05-10 12:56:35 +0100

    Removed passport middleware

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
-const passport = require('passport');
+const session = require('express-session')
 const mongoose = require('mongoose');
 require('dotenv').config()
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const bcrypt = require('bcryptjs');
 
 const mongoDB = process.env.DB_URL
 mongoose.connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true});
@@ -14,12 +14,16 @@ let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 var indexRouter = require('./routes/index');
+const UserModel = require('./models/UserModel');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
